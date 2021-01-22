ef0fdd254a41568645a810ee4fe4d5b659a9571a
Bart Jacobs <60138978+Cloudsparkle@users.noreply.github.com>
2021-01-22 16:48:07 +0100

    Fixes

--- RestoreCurrentQVSettings.ps1
+++ RestoreCurrentQVSettings.ps1
@@ -1,65 +1,98 @@
-﻿Add-PSSnapin Citrix*
+#requires -modules ActiveDirectory
+<#
+.SYNOPSIS
+  Perform restore of QlikView Settings after Current profile reset
+.DESCRIPTION
+  This script is part of a set to reset and backup/restore a Citrix userprofile. This is the part to restore QlikView settings on the current environment
+.INPUTS
+  User from AD Group
+.OUTPUTS
+  None
+.NOTES
+  Version:        1.0
+  Author:         Bart Jacobs - @Cloudsparkle
+  Creation Date:  19/01/2021
+  Purpose/Change: Current Citrix QlikView Settings profile restore
+.EXAMPLE
+  None
+#>
+
 $CTXDDC = "nitcitddc1vp"
+
+#Try loading Citrix CVAD Powershell modules, exit when failed
+if ((Get-PSSnapin "Citrix.Broker.Admin.*" -EA silentlycontinue) -eq $null)
+{
+  try {Add-PSSnapin Citrix* -ErrorAction Stop }
+	catch {Write-error "Error loading Citrix CVAD Powershell snapin"; Return }
+}
+
+#Initialize
+$CurrentQVRestoreGroup = "EMEA_Current-RestoreQVSettings"
+$CurrentQVRestoreGroupDone = "EMEA_Current-RestoreQVSettingsDone"
+
+$CurrentProfileShare = "\\nittoeurope.com\NE\Profiles\"
+$CurrentResetLogPath = $CurrentProfileShare + "0. Resetlog\"
+$QVINIPath = "\UPM_Profile\AppData\Roaming\QlikTech\QlikView\settings.ini"
+$QVINI = "settings.ini"
+$QVSettingsPath = "\UPM_Profile\AppData\Roaming\QlikTech\QlikView\"
 
 while ($true)
 {
-write-host "Cleaning up first..."
-[System.GC]::Collect()
-Sleep 15
+  $QVUsers = Get-ADGroupMember -Identity $CurrentQVRestoreGroup
+  foreach ($QVUser in $QVUsers)
+  {
+    Write-Host "Processing " $QVUser.name -ForegroundColor Yellow
 
-$QVUsers = Get-ADGroupMember -Identity FUJ_NE_CTX_MIGRATE-QV-SETTINGS
-foreach ($QVUser in $QVUsers)
-{
-Write-Host "Processing " $QVUser.name -ForegroundColor Yellow
+    $Currentsession = ""
+    $Currentsession = Get-XASession | select Accountname | where {$_.Accountname -like ("*"+$QVUser.SamAccountName)}
 
-$Currentsession = ""
-$Currentsession = Get-BrokerSession -AdminAddress $ctxddc -UserSID $QVUser.SID
-
-if ($Currentsession -ne $null)
+    if ($Currentsession -ne $null)
     {
-    write-host "User" $QVUser.name "has a current session. Moving on." -ForegroundColor Red
-    continue
+      write-host "User" $QVUser.name "has a current session. Moving on." -ForegroundColor Red
+      continue
     }
 
-#$ADUser = Get-ADUser $QVUser | select samaccountname
+    $RestoreLogID = Get-ChildItem $CurrentResetLogPath | select name | where {$_.name -like ($QVUser.SamAccountName+"*")}
 
-$Legacypath = "\\nittoeurope.com\NE\Profiles\" + $QVUser.samaccountname + "\UPM_Profile\AppData\Roaming\QlikTech\QlikView\*.ini"
-$FJPath = "\\nitctxfil1vp.nittoeurope.com\profiles$\"+ $QVUser.samaccountname + ".nittoeurope\UPM_Profile\AppData\Roaming\QlikTech\QlikView"
-$FJXMLPath = "\\nitctxfil1vp.nittoeurope.com\profiles$\"+ $QVUser.samaccountname + ".nittoeurope\UPM_Profile\AppData\Roaming\QlikTech\QlikView\settings.ini"
+    $Backuppath = $CurrentProfileShare + $RestoreLogID.Name + $QVINIPath
+    $CurrentPath = $CurrentProfileShare + $QVUser.SamAccountName + ".nittoeurope" + $QVSettingsPath
+    $CurrentINIFile1 = $Currentpath + $QVINI
 
-$LegacyExists =Test-Path -Path $Legacypath
-$FJExists =Test-Path -Path $FJPath
+    $BackupExists =Test-Path -Path $Backuppath
+    $CurrentExists =Test-Path -Path $CurrentPath
 
-#Write-Host $Legacypath, $LegacyExists, $FJExists
-
-if ($LegacyExists -eq $true)
+    if ($BackupExists)
     {
-    Write-host "Legacy settings exist..."
+      Write-host "Settings backup exists..."
 
-    if ($FJExists -eq $true)
-        {
-        Write-host "User has accessed the FJ application already."
+      if ($CurrentExists)
+      {
+        Write-host "User has accessed the Current application already."
         Write-Host "Copying files..."
-        Copy-Item $Legacypath -Destination $FJPath
+        Copy-Item $Backuppath -Destination $CurrentPath
         Write-Host "Fixing permissions..."
-        icacls $FJXMLPath /setowner $QVUser.samaccountname
-        icacls $FJXMLPath /inheritancelevel:e
-                
-        Write-Host "Removing user from AD Group" -ForegroundColor Green
-        Remove-ADGroupMember -Identity FUJ_NE_CTX_MIGRATE-QV-SETTINGS -Members $QVUser.samaccountname -Confirm:$False
-        }
-    Else
+        $INI1exists = Test-Path -Path $CurrentINIFile1
+        if ($INI1exists)
         {
-        write-host "User has not launched the FJ application yet. Moving on." -ForegroundColor red
+          icacls $CurrentINIFile1 /setowner $QVUser.samaccountname
+          icacls $CurrentINIFile1 /inheritancelevel:e
         }
+        Write-Host "Restore Complete Removing user from AD Group" -ForegroundColor Green
+        Remove-ADGroupMember -Identity $CurrentQVRestoreGroup -Members $QVUser.samaccountname -Confirm:$False
+        Add-ADGroupMember -Identity $CurrentQVRestoreGroupDone -Members $QVUser.samaccountname
+      }
+      Else
+      {
+        write-host "User has not launched the Current application yet. Moving on." -ForegroundColor red
+      }
     }
+  }
 
-
-}
-
-Write-Host "Waiting for next run..."
-clear-variable -name QVusers
-[System.GC]::GetTotalMemory($true) | out-null
-Sleep 60
-
+  Write-Host "Waiting for next run..."
+  clear-variable -name QVUsers
+  "Memory used before collection: $([System.GC]::GetTotalMemory($false))"
+  [System.GC]::Collect()
+  Sleep 15
+  "Memory used after full collection: $([System.GC]::GetTotalMemory($true))"
+  Sleep 15
 }
--- RestoreCurrentSAPSettings.ps1
+++ RestoreCurrentSAPSettings.ps1
@@ -1,14 +1,30 @@
-﻿#requires -modules ActiveDirectory
-#
+#requires -modules ActiveDirectory
+<#
+.SYNOPSIS
+  Perform restore of SAP Settings after Current profile reset
+.DESCRIPTION
+  This script is part of a set to reset and backup/restore a Citrix userprofile. This is the part to restore SAP settings on the current environment
+.INPUTS
+  User from AD Group
+.OUTPUTS
+  None
+.NOTES
+  Version:        1.0
+  Author:         Bart Jacobs - @Cloudsparkle
+  Creation Date:  19/01/2021
+  Purpose/Change: Current Citrix SAP Settings profile restore
+.EXAMPLE
+  None
+#>
+
 $CTXDDC = "nitcitddc1vp"
-#Initialize script variables
 
 #Try loading Citrix CVAD Powershell modules, exit when failed
 if ((Get-PSSnapin "Citrix.Broker.Admin.*" -EA silentlycontinue) -eq $null)
-  {
-	try {Add-PSSnapin Citrix* -ErrorAction Stop }
+{
+  try {Add-PSSnapin Citrix* -ErrorAction Stop }
 	catch {Write-error "Error loading Citrix CVAD Powershell snapin"; Return }
-  }
+}
 
 #Initialize
 $CurrentSAPRestoreGroup = "EMEA_Current-RestoreSAPSettings"
@@ -23,72 +39,69 @@ $SAPNWBCFavorites = "NWBCFavorites.xml"
 
 while ($true)
 {
+  $SAPUsers = Get-ADGroupMember -Identity $CurrentSAPRestoreGroup
+  foreach ($SAPUser in $SAPUsers)
+  {
+    Write-Host "Processing " $SAPUser.name -ForegroundColor Yellow
 
+    $Currentsession = ""
+    $Currentsession = Get-XASession | select Accountname | where {$_.Accountname -like ("*"+$SAPUser.SamAccountName)}
 
-$SAPUsers = Get-ADGroupMember -Identity $CurrentSAPRestoreGroup
-foreach ($SAPUser in $SAPUsers)
-{
-Write-Host "Processing " $SAPUser.name -ForegroundColor Yellow
-
-$Currentsession = ""
-$Currentsession = Get-XASession | select Accountname | where {$_.Accountname -like ("*"+$SAPUser.SamAccountName)}
-
-if ($Currentsession -ne $null)
+    if ($Currentsession -ne $null)
     {
-    write-host "User" $SAPUser.name "has a current session. Moving on." -ForegroundColor Red
-    continue
+      write-host "User" $SAPUser.name "has a current session. Moving on." -ForegroundColor Red
+      continue
     }
-$RestoreLogID = Get-ChildItem $CurrentResetLogPath | select name | where {$_.name -like ($SAPUser.SamAccountName+"*")}
 
-$Backuppath = $CurrentProfileShare + $RestoreLogID.Name + $SAPNWBCXMLPath
-$CurrentPath = $CurrentProfileShare + $SAPUser.SamAccountName + ".nittoeurope" + $SAPNWBCSettingsPath
-$CurrentXMLFile1 = $Currentpath + $SAPBCFavorites
-$CurrentXMLFile2 = $Currentpath + $SAPNWBCFavorites
+    $RestoreLogID = Get-ChildItem $CurrentResetLogPath | select name | where {$_.name -like ($SAPUser.SamAccountName+"*")}
 
-$BackupExists =Test-Path -Path $Backuppath
-$CurrentExists =Test-Path -Path $CurrentPath
+    $Backuppath = $CurrentProfileShare + $RestoreLogID.Name + $SAPNWBCXMLPath
+    $CurrentPath = $CurrentProfileShare + $SAPUser.SamAccountName + ".nittoeurope" + $SAPNWBCSettingsPath
+    $CurrentXMLFile1 = $Currentpath + $SAPBCFavorites
+    $CurrentXMLFile2 = $Currentpath + $SAPNWBCFavorites
 
-if ($BackupExists -eq $true)
+    $BackupExists =Test-Path -Path $Backuppath
+    $CurrentExists =Test-Path -Path $CurrentPath
+
+    if ($BackupExists)
     {
-    Write-host "Backup settings exist..."
+      Write-host "Settings backup exists..."
 
-    if ($CurrentExists -eq $true)
-        {
+      if ($CurrentExists)
+      {
         Write-host "User has accessed the Current application already."
         Write-Host "Copying files..."
         Copy-Item $Backuppath -Destination $CurrentPath
         Write-Host "Fixing permissions..."
         $XML1exists = Test-Path -Path $CurrentXMLFile1
-        if ($XML1exists -eq $true)
-            {
-            icacls $CurrentXMLFile1 /setowner $SAPUser.samaccountname
-            icacls $CurrentXMLFile1 /inheritancelevel:e
-            }
+        if ($XML1exists)
+        {
+          icacls $CurrentXMLFile1 /setowner $SAPUser.samaccountname
+          icacls $CurrentXMLFile1 /inheritancelevel:e
+        }
         $XML2exists = Test-Path -Path $CurrentXMLFile2
-        if ($XML2exists -eq $true)
-            {
-            icacls $CurrentXMLFile2 /setowner $SAPUser.samaccountname
-            icacls $CurrentXMLFile2 /inheritancelevel:e
-            }
-        
+        if ($XML2exists)
+        {
+          icacls $CurrentXMLFile2 /setowner $SAPUser.samaccountname
+          icacls $CurrentXMLFile2 /inheritancelevel:e
+        }
+
         Write-Host "Restore Complete Removing user from AD Group" -ForegroundColor Green
         Remove-ADGroupMember -Identity $CurrentSAPRestoreGroup -Members $SAPUser.samaccountname -Confirm:$False
         Add-ADGroupMember -Identity $CurrentSAPRestoreGroupDone -Members $SAPUser.samaccountname
-        }
-    Else
-        {
+      }
+      Else
+      {
         write-host "User has not launched the Current application yet. Moving on." -ForegroundColor red
-        }
+      }
     }
-    
-}
+  }
 
-Write-Host "Waiting for next run..."
-clear-variable -name SAPUsers
+  Write-Host "Waiting for next run..."
+  clear-variable -name SAPUsers
   "Memory used before collection: $([System.GC]::GetTotalMemory($false))"
   [System.GC]::Collect()
   Sleep 15
   "Memory used after full collection: $([System.GC]::GetTotalMemory($true))"
   Sleep 15
-
 }
